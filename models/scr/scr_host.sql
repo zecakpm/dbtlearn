6a1f729da32b8aaa6a5954502f8911b268dc416c
zecakpm <joseformiga@ymail.com>
2024-11-24 10:18:54 +0100

    source models

---
+++ models/scr/scr_host.sql
@@ -0,0 +1,11 @@
+-- import raw_hosts
+WITH raw_hosts AS (
+        SELECT * FROM AIRBNB.RAW.RAW_HOSTS
+)
+SELECT 
+    id AS host_id,
+    name AS host_name,
+    is_superhost,
+    created_at,
+    updated_at
+FROM raw_hosts
